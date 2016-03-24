ad4f9c4857360e734e51cfda2dc17c1a0c1d1baa
codebud7 <spuskeiler@gmail.com>
2016-03-24 20:00:49 +0100

    * initial commit of app

---
+++ src/main/java/com/codebud7/BerryshotApplication.java
@@ -0,0 +1,12 @@
+package com.codebud7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerryshotApplication.class, args);
+	}
+}
